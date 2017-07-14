6c654d71a5f92586d33563677946dd05b5c2e03f
jag10 <jota259@gmail.com>
2017-07-14 16:49:00 +0200

    remove weather.py (testing file)

--- weather.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import subprocess
-
-cmd = "git log --since='6am' --oneline | wc -l"
-ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-output = ps.communicate()[0]
-print(str(int(output)))
